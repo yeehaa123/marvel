36f553d896e3c50cb636db84ca91eea5239dc4a9
Jan Hein Hoogstad <yeehaa123@mac.com>
2015-03-03 17:47:05 +0100

    second version

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,20 +1,76 @@
 (function(){
+
+  var currentHero = {};
+  var allCharacters;
+
+  function setCurrentHero(hero){
+    currentHero = hero;
+    render(allCharacters, currentHero);
+  }
+
   window.UI = {
-    render: renderItems 
+    render: render
   };
 
-  function renderItems(characters){
-    _.each(characters, function(character){
-      renderItem(character);
-    });
+  var Item = React.createClass({
+    handleClick: function(){
+      currentHero = setCurrentHero(this.props.character);
+    },
+    render: function(){
+      var character = this.props.character;
+      return (
+        React.createElement('li', { onClick: this.handleClick }, 
+          React.createElement('img', {src: character.url}),
+          React.createElement('p', null, character.name)
+        )
+      )
+    }
+  });
+
+  var Sidebar = React.createClass({
+    render: function(){
+      var list = _.map(this.props.characters, function(character, index){
+        return React.createElement(Item, { key: index, character: character })
+      });
+      return (
+        React.createElement('section', {id: 'sidebar'},
+          React.createElement('ul', null, list)
+        )
+      )
+    }
+  });
+
+  var Main = React.createClass({
+    render: function(){
+      var name = this.props.hero ? this.props.hero.name : '';
+      if(!name){
+        React.createElement('section', { id: 'main' })
+      }
+      return (
+        React.createElement('section', { id: 'main' },
+          React.createElement('h1', null, name),
+          React.createElement('h2', null, this.props.hero)
+        )
+      )
+    }
+  });
+
+  var App = React.createClass({
+    render: function(){
+      return (
+        React.createElement('div', {id: 'content' }, 
+          React.createElement(Sidebar, { characters: this.props.characters }),
+          React.createElement(Main,  { hero: this.props.hero})
+      ))
+    }
+  });
+
+  function render(characters, currentHero){
+    allCharacters = characters;
+    React.render(
+      React.createElement(App, { characters: characters, hero: currentHero}),
+      document.querySelector('#content')
+    );
   }
 
-  function renderItem(character){
-    var template = document.querySelector('#names');
-    var insertionPoint = document.querySelector('#list');
-    var clone = document.importNode(template.content, true);
-    clone.querySelector('p').textContent = character.name;
-    clone.querySelector('img').src = character.url; 
-    insertionPoint.appendChild(clone);
-  }
 })();
